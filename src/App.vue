7c595c8125ad8e423923d8ee6e08bc83d3be894d
albericobenhur <sudbenhur@gmail.com>
2021-11-02 13:57:38 -0300

    iniciando

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <main class="columns is-gapless is-multiline"></main>
+  <div class="is-flex">
+    <div class="column is-one-quarter">
+      <BarraLateral />
+    </div>
+
+    <div class="column is-three-quarter">
+      <Formulario />
+      <!-- lista de tarefas ??-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Formulario from "./components/Formulario.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+     BarraLateral,
+     Formulario 
+  }
+});
+</script>
+
+<style>
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulario para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa voce deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  name: "Formulario",
+  components: {
+    Temporizador,
+  },
+  data() {
+    return {
+      descricao: "",
+    };
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido: number): void {
+      console.log("tempo da tarefa", tempoDecorrido);
+      console.log('descrição da tarefa', this.descricao)
+      this.descricao =''
+    },
+  },
+});
+</script>
